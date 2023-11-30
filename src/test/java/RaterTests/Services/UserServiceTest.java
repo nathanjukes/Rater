a69d8e21f47ff0ec4b8a1a62f209431c0a44fac1
nathanjukes <njukes1@gmail.com>
2023-11-30 17:06:30 +0000

    user service tests

---
+++ src/test/java/RaterTests/Services/UserServiceTest.java
@@ -0,0 +1,82 @@
+package RaterTests.Services;
+
+import Rater.Models.Org.Org;
+import Rater.Models.User.OrgUserCreateRequest;
+import Rater.Models.User.User;
+import Rater.Models.User.UserCreateRequest;
+import Rater.Repositories.UserRepository;
+import Rater.Services.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserServiceTest {
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void testGetUser() {
+        UUID userId = UUID.randomUUID();
+        userService.getUser(userId);
+
+        verify(userRepository, times(1)).findById(eq(userId));
+    }
+
+    @Test
+    public void testGetByEmail() {
+        String email = "testEmail";
+        userService.getUserByEmail(email);
+
+        verify(userRepository, times(1)).findByEmail(eq(email));
+    }
+
+    @Test
+    public void testGetUsers() {
+        userService.getUsers();
+
+        verify(userRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void testCreateUser() {
+        String email = "testEmail";
+        String password = "testPassword";
+        Org org = new Org("testOrg");
+
+        UserCreateRequest userCreateRequest = new UserCreateRequest(email, password, org.getName());
+
+        userService.createUser(userCreateRequest, org, passwordEncoder);
+
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    public void testCreateOrgUser() {
+        String email = "testEmail";
+        String password = "testPassword";
+        Org org = new Org("testOrg");
+
+        OrgUserCreateRequest userCreateRequest = new OrgUserCreateRequest(email, password, UUID.randomUUID());
+
+        userService.createUser(userCreateRequest, org, passwordEncoder);
+
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+}
